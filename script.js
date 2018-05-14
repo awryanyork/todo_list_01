58094c74783e1d5c722f0cc986f6a2bb6d322d50
Ryan <awryanyork@gmail.com>
2018-05-14 14:53:57 -0500

    - Added hideCompleted button - Added some comments to explain purposes for various functions, methods, and event listeners

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const toggleAllButton = document.querySelector('.toggleAllButton');
+const hideCompletedButton = document.querySelector('.hideCompletedButton');
 const todoInput = document.querySelector('.todoInput');
 const form = document.querySelector('form');
 const todosUl = document.querySelector('ul');
@@ -44,6 +45,14 @@ const handlers = {
       todoWrap.appendChild(todoItem);
       todoWrap.appendChild(todoDeleteButton);
 
+      const todoIsCompleted = todoWrap.classList.contains('true');
+      const completedAreHidden = hideCompletedButton.classList.contains('hiding');
+
+      if (todoIsCompleted && completedAreHidden) {
+        todoWrap.classList.add('hide');
+      }
+
+
       // add todo to the unordered list
       todosUl.appendChild(todoWrap);
     });
@@ -76,22 +85,30 @@ const view = {
       todos.forEach(todo => {
         todo.completed = false;
       });
+      // take away styling indicating at least one completed todo is being hidden
+      hideCompletedButton.classList.remove('hiding');
     }
+
     handlers.displayTodos();
     view.handleCheckboxCheckedStatus();
   }, // end of 'toggleAll' method
   handleChecboxClick: function (e) {
     const clickedACheckbox = e.target.type === 'checkbox';
     if (clickedACheckbox) {
-      const markedCompleted = e.target.checked === true;
-      if (markedCompleted) {
+      const isMarkedCompleted = e.target.checked === true;
+      if (isMarkedCompleted) {
         e.target.parentNode.classList.add('true');
+        e.target.parentNode.classList.remove('false');
       } else {
         e.target.parentNode.classList.remove('true');
+        e.target.parentNode.classList.add('false');
       }
     }
   }, // end of 'handleChecked' method
   handleCheckboxCheckedStatus: function () {
+    /* this makes a checkbox checked if the todo is marked 
+       complete by an action besides clicking the 
+       checkbox (e.g. using the 'toggleAll' button) */
     const todosNodeList = document.querySelectorAll('.todoWrap');
     todosNodeList.forEach(node => {
       if (node.classList.contains('true')) {
@@ -100,18 +117,40 @@ const view = {
         node.firstChild.checked = false;
       }
     });
-  }
+  }, // end of 'handleCheckboxCheckedStatus' method
+  toggleHideCompleted: function () {
+    todosUl.childNodes.forEach(todo => {
+      const todoIsCompleted = todo.classList.contains('true');
+      if (todoIsCompleted) {
+        todo.classList.toggle('hide');
+      }
+    });
+    const todoDivsArray = Array.from(todosUl.childNodes);
+    const atLeastOneTodoHidden = todoDivsArray.some(todo => {
+      return todo.classList.contains('hide');
+    });
+    if (atLeastOneTodoHidden) {
+      hideCompletedButton.classList.add('hiding');
+    } else {
+      hideCompletedButton.classList.remove('hiding');
+    }
+  } // end of 'toggleHideCompleted' method
 }; // end of 'view' object
 
-// add todo to the todos array and display it's contents on the page
+// displays all the todos in the 'todos' array on the page
 form.addEventListener('submit', view.displayTodos);
 
-// delete todo
+// permantly deletes a todo
 todosUl.addEventListener('click', view.deleteTodo);
 
-// toggle all 
+// toggles all todos either complete or incomplete
+// also handles the case of someone marking a todo complete/incomplete via the 'toggleAll' button
 toggleAllButton.addEventListener('click', view.toggleAll);
 
-// handlechecked
+// handles the case of someone marking a todo complete/incomplete via the checkbox
 todosUl.addEventListener('click', view.handleChecboxClick);
+
+/* hides all completed todos and updates the styling of the
+ 'hideCompletedButton' to reflect that todos are being hidding */
+hideCompletedButton.addEventListener('click', view.toggleHideCompleted);
 
